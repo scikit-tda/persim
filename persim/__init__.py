889ac34714b69fd5c098c79e17ca0746aa165d5f
Nathaniel Saul <nat@saulgill.com>
2018-07-29 19:10:05 -0700

    setup one true version number

--- persim/__init__.py
+++ persim/__init__.py
@@ -1,6 +1,3 @@
 from .persim import PersImage
 
-# Enable access to version number
-import pkg_resources
-
-__version__ = pkg_resources.get_distribution("persim").version
+from ._version import __version__
