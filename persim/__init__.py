2b1ba3763bdcadab654fbe5650c8865d6589e898
Michael Catanzaro <catanzaromj@gmail.com>
2021-02-03 19:56:54 -0600

    Refactor transformer
    - Rename transformer file
    - Rename `PLA` to `PersistenceLandscaper`, remove `PLE`
    - Add `flatten`, `start`, and `stop` parameters to init
    - Add documentation and examples

--- persim/__init__.py
+++ persim/__init__.py
@@ -7,7 +7,7 @@ from .gromov_hausdorff import *
 from .visuals import *
 from .pers_landscape_exact import *
 from .pers_landscape_approx import *
-from .pers_landscape_transf import *
+from .pers_landscape_transformer import *
 from .pers_landscape_aux import *
 from .pers_landscape_visual import *
 
